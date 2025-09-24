75a8cb8c4029829fe4af1f2bc9bab7998380e225
nabidam <navidabbaspoor@gmail.com>
2025-09-24 20:40:10 +0330

    ziggy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -19,5 +20,10 @@ export default defineConfig({
     ],
     esbuild: {
         jsx: 'automatic',
+    },
+    resolve: {
+        alias: {
+            'ziggy-js': path.resolve('vendor/tightenco/ziggy'),
+        },
     },
 });
