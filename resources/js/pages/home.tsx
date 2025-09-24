e66dd7178983b84bc5af2821d2ef1cdf90279fa9
nabidam <navid.abbaspoor@gmail.com>
2025-09-24 13:22:41 +0330

    change home

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,9 +1,9 @@
 import { Head } from '@inertiajs/react';
 
-export default function Welcome() {
+export default function Home() {
     return (
         <>
-            <Head title="Welcome">
+            <Head title="Home">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get('/', [TodoController::class, 'index'])->name('home');
