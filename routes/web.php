650648e1a3791eadcb2518d08f9ae8019a5f7d40
nabidam <navidabbaspoor@gmail.com>
2025-09-24 20:39:56 +0330

    web routes

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TodoController::class, 'index'])->name('home');
+// Route::delete('/', [TodoController::class, 'index'])->name('home');
+
+Route::resource('todo', TodoController::class);
