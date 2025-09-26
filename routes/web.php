5f34928b0613f5172b3944aa2863ae2a04428e55
nabidam <navidabbaspoor@gmail.com>
2025-09-26 18:27:14 +0330

    api routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TodoController::class, 'index'])->name('home');
-// Route::delete('/', [TodoController::class, 'index'])->name('home');
-
-Route::resource('todo', TodoController::class);
+Route::post('/', [TodoController::class, 'store'])->name('store');
+Route::patch('/{todo}/done', [TodoController::class, 'toggleDone'])->name('done');
+Route::delete('/{todo}', [TodoController::class, 'destroy'])->name('destroy');
